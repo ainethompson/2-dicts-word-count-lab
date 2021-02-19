61030b0afc3817ddcd423a6b93c1a84c94778b25
Áine Thompson <ainegthompson@gmail.com>
2021-02-19 01:37:48 +0000

    Remove punctuation and capitalization

--- wordcount.py
+++ wordcount.py
@@ -1,18 +1,15 @@
 # put your code here.
 
-
 def count_words(filename):
 
-    chars_to_replace = ["/n",]
+    punctuation = ''',.?!()-[]{};:'"\<>/@#$&%^~_'''
     word_counts = {}
 
-    with open(filename) as f:
-
-        lines = f.readlines()
-    for line in lines:
+    for line in open(filename):
         words = line.rstrip().split(' ')
 
         for word in words:
+            word = ''.join(char for char in word if char not in punctuation).lower()
             word_counts[word] = word_counts.get(word, 0) + 1
 
     for word, count in word_counts.items():
