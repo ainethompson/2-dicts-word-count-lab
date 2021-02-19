4b59a28cebeeef6e9985dc7e62ce277dc8eed64d
Áine Thompson <ainegthompson@gmail.com>
2021-02-19 01:03:56 +0000

    Complete basic count_words function

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,21 @@
 # put your code here.
+
+
+def count_words(filename):
+
+    chars_to_replace = ["/n",]
+    word_counts = {}
+
+    with open(filename) as f:
+
+        lines = f.readlines()
+    for line in lines:
+        words = line.rstrip().split(' ')
+
+        for word in words:
+            word_counts[word] = word_counts.get(word, 0) + 1
+
+    for word, count in word_counts.items():
+        print(word, count)
+
+count_words("test.txt")
